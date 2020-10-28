a7c3b5d567319d3479029e383d6577538279c06d
Umar Ahmedov <ahmuy002@mymail.unisa.edu.au>
2020-10-29 01:00:13 +1030

    created file management

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+import utilities
 
 app = Flask(__name__)
 
+app.config["UPLOAD_DIR"] = os.path.abspath(os.curdir) + "/static/uploads"
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        if request.files:
+            file_name = request.form['fileName']
 
+            file = request.files["file"]
+            file.save(os.path.join(app.config["UPLOAD_DIR"], file_name + '.' + utilities.get_file_extension(file.filename)))
+
+            return redirect(request.url)
+
+    return render_template("index.html", file_list=utilities.get_all_uploaded_files())
+
+@app.route("/delete_file/<file_id>")
+def delete_file(file_id):
+    utilities.delete_file(file_id)
+    return render_template("index.html", file_list=utilities.get_all_uploaded_files())
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
---
+++ utilities.py
@@ -0,0 +1,30 @@
+import os
+from main import app
+
+UPLOAD_DIR = app.config["UPLOAD_DIR"]
+
+
+def get_file_extension(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower()
+
+
+def get_all_uploaded_files():
+    files_in_directory = os.listdir(UPLOAD_DIR)
+    files = []
+
+    for index, file in enumerate(files_in_directory):
+        files.append(
+            {
+                'id': index,
+                'name': os.path.splitext(file)[0],
+                'extension': os.path.splitext(file)[1][1:].upper(),
+                'size': os.stat(os.path.join(UPLOAD_DIR, file)).st_size
+            }
+        )
+
+    return files
+
+def delete_file(id):
+    files_in_directory = os.listdir(UPLOAD_DIR)
+    os.remove(os.path.join(UPLOAD_DIR, files_in_directory[int(id)]))
